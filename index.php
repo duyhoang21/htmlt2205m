9a52a4af95a551edcf5063de564ea7c4f455f46d
Nguyen Duy Hoang <hoangnd@fpt.edu.vn>
2022-07-15 09:22:28 +0700

    add home page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h2>Hello world</h2>
+    <a href="html_my-webpage.html">Go to MARS</a>
+</body>
+</html>
